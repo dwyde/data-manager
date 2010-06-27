41fbd3e25a61208aee9190757fc579dfced96fbb
david <david@myhost.localdomain>
2010-06-27 21:49:37 +0000

    Deleted unused code in couch_backend.py

--- data/couch_backend.py
+++ data/couch_backend.py
@@ -3,23 +3,6 @@ from couchdb import Document
 from couchdb.schema import *
 import re
 
-class School(Document):
-    name = TextField()
-    specialties = ListField(TextField)
-    gre_general = BooleanField()
-    gre_subject = BooleanField()
-    deadline = DateField()
-    contact = TextField()
-    rank = IntegerField()
-    notes = ListField(TextField)
-
-class Job(Document):
-    name = TextField()
-    location = TextField()
-    deadline = DateField()
-    contact = TextField()
-    notes = ListField(TextField)
-
 class CouchBackend:
     bad_dbname_chars = r'[^-_$()+/a-z0-9]'
     bad_docname_chars = r'\W'
@@ -45,13 +28,3 @@ class CouchBackend:
             data = [row.doc.get(x[0]) for x in fields]
             yield map(str, data)
             
-if __name__ == '__main__':
-    #couch = couchdb.Server()
-    #db = couch.create('test')
-    #db = couch['test']
-    ##s = School(name='Test', specialties=['science', 'math'], gre_general=True, gre_subject=False,
-    ##deadline=None, rank=8) #id='unique'
-    #s.store(db)
-    #
-    #couch.delete('test')
-    pass
